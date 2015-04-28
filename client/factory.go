2efb9cb690533cb6e92817736f6ddf24b0803543
Derek McGowan <derek@mcgstyle.net>
2015-04-27 17:43:18 -0700

    client: Add factory and update dist pull

---
+++ client/factory.go
@@ -0,0 +1,77 @@
+package client
+
+import (
+	"errors"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/context"
+
+	"github.com/docker/distribution"
+	"github.com/docker/distribution/namespace"
+	rclient "github.com/docker/distribution/registry/client"
+)
+
+// RepositoryClientConfig is used to create new clients from endpoints
+type RepositoryClientConfig struct {
+	TrimHostname bool
+	AllowMirrors bool
+	Header       http.Header
+
+	NamespaceFile string
+	// Discovery method
+
+	// Store auth configurations
+}
+
+// Resolver returns a new namespace resolver using this repository
+// client configuration.  If there is an error loading the configuration,
+// an error will be returned with a nil resolver.
+func (f *RepositoryClientConfig) Resolver() (namespace.Resolver, error) {
+	// Read base entries from f.NamespaceFile
+	nsf, err := os.Open(f.NamespaceFile)
+	if err != nil {
+		return nil, err
+	}
+
+	entries, err := namespace.ReadEntries(nsf)
+	if err != nil {
+		return nil, err
+	}
+
+	resolver := namespace.NewNamespaceResolver(entries, namespace.NopDiscoverer{}, f.newRepository)
+
+	return resolver, nil
+}
+
+// type RepositoryClientFactory func(version string, registries, mirrors []string) (distribution.Repository, error)
+func (f *RepositoryClientConfig) newRepository(namespace string, registries, mirrors []string) (distribution.Repository, error) {
+	if f.TrimHostname {
+		i := strings.IndexRune(namespace, '/')
+		if i > -1 && i < len(namespace)-1 {
+			// TODO(dmcgowan): Check if first element is actually hostname
+			namespace = namespace[i+1:]
+		}
+
+	}
+
+	// Currently only single endpoint repository used
+	endpoint := &rclient.RepositoryEndpoint{
+		Header: f.Header,
+	}
+
+	if f.AllowMirrors && len(mirrors) > 0 {
+		endpoint.Endpoint = mirrors[0]
+		endpoint.Mirror = true
+	}
+	if endpoint.Endpoint == "" && len(registries) > 0 {
+		endpoint.Endpoint = registries[0]
+	}
+
+	if endpoint.Endpoint == "" {
+		return nil, errors.New("No valid endpoints")
+	}
+
+	return rclient.NewRepositoryClient(context.Background(), namespace, endpoint)
+}
--- cmd/dist/pull.go
+++ cmd/dist/pull.go
@@ -1,6 +1,14 @@
 package main
 
-import "github.com/codegangsta/cli"
+import (
+	"log"
+	"strings"
+
+	"golang.org/x/net/context"
+
+	"github.com/codegangsta/cli"
+	"github.com/docker/distribution/client"
+)
 
 var (
 	commandPull = cli.Command{
@@ -10,12 +18,73 @@ var (
 		Flags: []cli.Flag{
 			cli.StringFlag{
 				Name:  "r,registry",
-				Value: "hub.docker.io",
+				Value: "docker.io",
 				Usage: "Registry to use (e.g.: localhost:5000)",
 			},
+			// Client Params
+			// TrimHostname?
+			// BaseNamespace File
+			// Auth Configuration
+			// Output Params
+			// Where to store pull
 		},
 	}
 )
 
+func splitTag(ns string) (string, string) {
+	nsSplit := ns
+	lastSlash := strings.LastIndex(nsSplit, "/")
+	if lastSlash > -1 {
+		nsSplit = nsSplit[lastSlash:]
+	}
+	lastColon := strings.LastIndex(nsSplit, ":")
+	if lastColon > -1 {
+		return ns[:lastSlash+lastColon], nsSplit[lastColon+1:]
+	}
+	return ns, "latest"
+}
+
 func imagePull(c *cli.Context) {
+	config := client.RepositoryClientConfig{
+		TrimHostname:  true,
+		AllowMirrors:  true,
+		NamespaceFile: ".namespace.cfg",
+		Header: map[string][]string{
+			"User-Agent": {"docker/1.6.0 distribution-cli"},
+		},
+	}
+
+	resolver, err := config.Resolver()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ns := range c.Args() {
+		name, tag := splitTag(ns)
+		log.Printf("Pulling %s %s", name, tag)
+		nspace, err := resolver.Resolve(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		repo, err := nspace.Repository(context.Background(), name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ms := repo.Manifests()
+		m1, err := ms.GetByTag(tag)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//ls := repo.Layers()
+		for _, layer := range m1.FSLayers {
+			// Parse blobSum
+			log.Printf("Pulling: %s", layer.BlobSum)
+		}
+		// Save manifest
+		// Save each layer
+		log.Printf("Manifest: %s", m1.Raw)
+
+	}
 }
